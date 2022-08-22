31f5a7a6270ec52942f1cb497c0de9ae9104e845
Gtorres21 <giovannytorres21@gmail.com>
2022-08-22 09:27:22 -0700

    Assignment Button Clicker Completed

---
+++ 03_JS/buttonclicker/script.js
@@ -0,0 +1,12 @@
+function logout(element){
+    element.innerText = "Logout";
+}
+
+
+function remove(element){
+    element.remove();
+}
+
+function alurt(){
+    alert("This page says Ninja was liked");
+}
